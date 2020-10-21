2ebb666b3c57dc71be075b8ea4edfa1e50d537c6
Alexander G. Yang <spark8402@rogers.com>
2020-10-20 22:28:56 -0400

    added freecodecamp challenge

--- FreeCodeCamp/wherefore_are_thou.js
+++ FreeCodeCamp/wherefore_are_thou.js
@@ -1,18 +1,27 @@
 function whatIsInAName(collection, source) {
-    var arr = [];
-    for(i=0; i < collection.length; i++){
-      for (const [key, value] of Object.entries(collection[i])) {
-        arr.push(`${key}:${value}`)
+
+  var srcKeys = Object.keys(source);
+
+  // filter the collection
+  return collection.filter(function(obj) {
+    for (var i = 0; i < srcKeys.length; i++) {
+      if (
+        !obj.hasOwnProperty(srcKeys[i]) ||
+        obj[srcKeys[i]] !== source[srcKeys[i]]
+      ) {
+        return false;
+      }
     }
-    console.log(arr)
- 
-  }
-  return arr;
+    return true;
+  });
 }
-  
-  console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
 
-  /*
-  whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" }) should return [{ first: "Tybalt", last: "Capulet" }]
-  */ 
-
+// test here
+console.log(whatIsInAName(
+  [
+    { first: "Romeo", last: "Montague" },
+    { first: "Mercutio", last: null },
+    { first: "Tybalt", last: "Capulet" }
+  ],
+  { last: "Capulet" }
+));
