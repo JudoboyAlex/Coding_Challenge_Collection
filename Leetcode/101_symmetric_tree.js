ffde9f40b712446865ca1e8b0a86b3709654d9d7
Alexander G. Yang <spark8402@rogers.com>
2021-01-22 21:03:03 -0500

    challenge completed

---
+++ Leetcode/101_symmetric_tree.js
@@ -0,0 +1,17 @@
+/*
+Runtime: 84 ms, faster than 91.66% of JavaScript online submissions for Symmetric Tree.
+Memory Usage: 40.2 MB, less than 90.11% of JavaScript online submissions for Symmetric Tree.
+*/
+
+var isSymmetric = function (root) {
+  return root == null || isSymmetricHelp(root.left, root.right);
+};
+
+var isSymmetricHelp = function (left, right) {
+  if (left == null || right == null) return left == right;
+  if (left.val != right.val) return false;
+  return (
+    isSymmetricHelp(left.left, right.right) &&
+    isSymmetricHelp(left.right, right.left)
+  );
+};
