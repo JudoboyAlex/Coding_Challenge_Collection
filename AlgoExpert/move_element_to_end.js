4a4c83c1f1d066e011e56284969e5de8dce24b31
Alexander G. Yang <spark8402@rogers.com>
2021-03-02 14:06:33 -0500

    another challenge added

---
+++ AlgoExpert/move_element_to_end.js
@@ -0,0 +1,18 @@
+// O(n) time | O(1) space
+
+function moveElementToEnd(array, toMove) {
+	let i = 0;
+	let j = array.length - 1;
+	while(i < j){
+		while(i < j && array[j] === toMove) j--;
+		if( array[i] === toMove) swap(i, j, array);
+		i++;
+	}
+	return array;
+}
+
+function swap(i, j, array){
+	const temp = array[j];
+	array[j] = array[i];
+	array[i] = temp;
+}
