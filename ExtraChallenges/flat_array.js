33210240ce6d1affdb11a6502cc25def435d35f2
Alexander G. Yang <spark8402@rogers.com>
2021-06-15 08:50:12 -0400

    added popular fe interview question

---
+++ ExtraChallenges/flat_array.js
@@ -0,0 +1,22 @@
+let array=[ 1, 2, 3,
+  [4, 5, 6],
+  7,
+  [8,
+    [9, 10, 11,
+      [12, 13, 14]
+] ],
+  [15, 16, 17, 18, 19,
+    [20, 21, 22,
+      [23, 24, 25,
+        [26, 27, 29]
+], 30, 31 ], 32
+], 33 ]
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    console.log(flat);
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
+
+console.log(flatten(array));
