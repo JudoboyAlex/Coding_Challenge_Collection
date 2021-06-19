b451b9a06afa8ca6ca5b10a70bd4e5cf216675f6
Alexander G. Yang <spark8402@rogers.com>
2021-06-19 14:45:10 -0400

    completed more resursion challenges

---
+++ JayWengrow/recursion/findX.js
@@ -0,0 +1,12 @@
+// Use recursion to write a function that accepts a string and returns the first index that contains the character “x.” For example, the string, "abcdefghijklmnopqrstuvwxyz" has an “x” at index 23. To keep things simple, assume the string definitely has at least one “x.”
+
+const findX=(str) => {
+
+  if(str[str.length-1] === "x"){
+    return str.length-1;
+  }
+  let newStr = str.slice(0, -1);
+  return findX(newStr);
+}
+
+console.log(findX("abcdefghijklmnopqrstuvwxyz"));
