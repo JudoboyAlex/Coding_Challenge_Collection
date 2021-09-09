94df34cb1f828a25eaaed6202ef997b7a5d8db07
Alexander G. Yang <spark8402@rogers.com>
2021-09-09 19:51:42 -0400

    new challenge completed

---
+++ GrokkingInterview/06_reverse_linked_list/reverse_linked_list.js
@@ -0,0 +1,41 @@
+class Node {
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
+
+  print_list() {
+    let temp = this;
+    while (temp !== null) {
+      process.stdout.write(`${temp.value} `);
+      temp = temp.next;
+    }
+    console.log();
+  }
+}
+
+
+function reverse(head) {
+  let current = head,
+    previous = null;
+  while (current !== null) {
+    next = current.next; // temporarily store the next node
+    current.next = previous; // reverse the current node
+    previous = current; // before we move to the next node, point previous to the current node
+    current = next; // move on the next node
+  }
+  return previous;
+}
+
+
+const head = new Node(2);
+head.next = new Node(4);
+head.next.next = new Node(6);
+head.next.next.next = new Node(8);
+head.next.next.next.next = new Node(10);
+
+process.stdout.write('Nodes of original LinkedList are: ');
+head.print_list();
+result = reverse(head);
+process.stdout.write('Nodes of reversed LinkedList are: ');
+result.print_list();
