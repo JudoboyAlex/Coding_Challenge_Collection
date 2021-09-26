f6be2ff9da368c2243af14d716eddec25a12c966
Alexander G. Yang <spark8402@rogers.com>
2021-09-26 09:32:48 -0400

    added blind challenge

---
+++ Blind_75/string/longest_repeating_character_replacement.js
@@ -0,0 +1,29 @@
+// Input: s = "AABABBA", k = 1
+// Output: 4
+// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+// The substring "BBBB" has the longest repeating letters, which is 4.
+
+const characterReplacement = (s, k) => {
+  let left = 0;
+  let right = 0;
+  let maxCharCount = 0;
+  const visited = {};
+
+  while (right < s.length) {
+    const char = s[right];
+    visited[char] = visited[char] ? visited[char] + 1 : 1;
+
+    if (visited[char] > maxCharCount) maxCharCount = visited[char];
+
+    if (right - left + 1 - maxCharCount > k) {
+      visited[s[left]]--;
+      left++;
+    }
+
+    right++;
+  }
+  
+  return right - left;
+};
+
+console.log(characterReplacement("AABABBA",1));
