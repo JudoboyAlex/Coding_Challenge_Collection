9fbda420f1a109ac60a7fc596783a2cb3a53fa8d
Alexander G. Yang <spark8402@rogers.com>
2021-10-09 20:48:59 -0400

    added famous spiral matrix

--- Blind_75/matrix/54_spiral_matrix.js
+++ Blind_75/matrix/54_spiral_matrix.js
@@ -1,6 +1,48 @@
 var spiralOrder = function(matrix) {
+  let result = []
+        
+  if (matrix.length == 0) {
+      return result;
+  }
   
-};
+  let rowBegin = 0;
+  let rowEnd = matrix.length-1;
+  let colBegin = 0;
+  let colEnd = matrix[0].length - 1;
+  
+  while (rowBegin <= rowEnd && colBegin <= colEnd) {
+      // Traverse Right
+      for (let j = colBegin; j <= colEnd; j++) {
+          result.push(matrix[rowBegin][j]);
+      }
+      rowBegin++;
+      
+      // Traverse Down
+      for (let j = rowBegin; j <= rowEnd; j++) {
+          result.push(matrix[j][colEnd]);
+      } 
+      colEnd--;
+      
+      if (rowBegin <= rowEnd) {
+          // Traverse Left
+          for (let j = colEnd; j >= colBegin; j--) {
+              result.push(matrix[rowEnd][j]);
+          }
+      }
+      rowEnd--;
+      
+      if (colBegin <= colEnd) {
+          // Traver Up
+          for (let j = rowEnd; j >= rowBegin; j --) {
+              result.push(matrix[j][colBegin]);
+          }
+      }
+      colBegin ++;
+  }
+  
+  return result;
+}
+
 
 
 console.log(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
--- FrontEndInterview/flat_array_es6.js
+++ FrontEndInterview/flat_array_es6.js
@@ -3,3 +3,20 @@ const flatArr = [].concat(...arr) // only works on 1 level
 
 console.log(flatArr)
 console.log(arr.flat(Infinity)) // built in flat function   
+
+function flatten(arr) {
+	return arr.reduce(function(prev, curr) {
+		if (Array.isArray(curr)) {
+			prev = prev.concat(flatten(curr));
+		} else {
+            prev.push(curr);
+		}
+		return prev;
+	}, []);
+}
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
