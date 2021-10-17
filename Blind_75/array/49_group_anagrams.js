e63226f663b44ad0747d2bc7cfafe8524a2ba84f
Alexander G. Yang <spark8402@rogers.com>
2021-10-16 21:48:17 -0400

    anagram solved

---
+++ Blind_75/array/49_group_anagrams.js
@@ -0,0 +1,17 @@
+const groupAnagrams = strs => {
+  const map = {};
+  
+  for (let str of strs) {
+      const key = [...str].sort().join('');
+
+      if (!map[key]) {
+          map[key] = [];
+      }
+
+      map[key].push(str);
+  }
+  
+  return Object.values(map);
+};
+
+console.log(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
