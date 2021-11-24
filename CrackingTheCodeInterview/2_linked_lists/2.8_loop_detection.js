57e06aa6a2dd29da9e90e9811f18cf6ed78a90b1
Alexander G. Yang <spark8402@rogers.com>
2021-11-23 23:02:32 -0500

    linked list ctci challenge added

---
+++ CrackingTheCodeInterview/2_linked_lists/2.8_loop_detection.js
@@ -0,0 +1,17 @@
+var detectCycle = function(head){
+  let slow = head;
+  let fast = head;
+  while(fast && fast.next && fast.next.next){
+      slow = slow.next;
+      fast = fast.next.next;
+      if(slow === fast){ 
+          slow = head;
+          while(slow !== fast){
+              slow = slow.next;
+              fast = fast.next;
+          }
+          return slow;
+      }
+  }
+  return null;
+}
