739eb4616e2d8aad8c743762913ddbd677900539
Alexander G. Yang <spark8402@rogers.com>
2021-12-09 18:14:58 -0500

    updated algoexpert tree challenge and updated recursion prompts

--- recursion-prompts/src/recursion.js
+++ recursion-prompts/src/recursion.js
@@ -40,18 +40,34 @@ var sum = function(array) {
 // 3. Sum all numbers in an array containing nested arrays.
 // arraySum([1,[2,3],[[4]],5]); // 15
 var arraySum = function(array) {
+  if (array.length === 0) return 0;
+  var sum = 0;
+  for (var i=0; i<array.length; i++) {
+    if (array[i] instanceof Array) {
+      sum += arraySum(array[i]);
+    } else {
+      sum += array[i];
+    }
+  }
+  return sum;
 };
 
 // 4. Check if a number is even.
 var isEven = function(n) {
+  n = Math.abs(n);
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(n-2);
 };
+
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
-};
-
+  if (n === 0) return 0;
+  return n > 0 ? n - 1 + sumBelow(n - 1) : n + 1 + sumBelow(n + 1);
+}
 // 6. Get the integers within a range (x, y).
 // range(2,9); // [3,4,5,6,7,8]
 var range = function(x, y) {
