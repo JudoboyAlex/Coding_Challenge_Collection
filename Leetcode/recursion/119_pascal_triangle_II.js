6daeee19c25379b9c615757763acc8d1086678b9
Alexander G. Yang <spark8402@rogers.com>
2021-12-21 08:27:58 -0500

    done recursion challenge

---
+++ Leetcode/recursion/119_pascal_triangle_II.js
@@ -0,0 +1,12 @@
+const getRow = rowIndex => {
+  const res = []
+  
+  while (res.length <= rowIndex) {
+      res.unshift(1)
+      for(let i = 1; i < res.length - 1; i++) {
+          res[i] += res[i + 1]
+      }
+  }
+  
+  return res
+}
