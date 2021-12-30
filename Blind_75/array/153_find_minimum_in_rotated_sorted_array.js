3ab95b6857dc6db802031d17793be35fde1b8ff9
Alexander G. Yang <spark8402@rogers.com>
2021-12-29 23:29:39 -0500

    another blind 75 challenge completed

---
+++ Blind_75/array/153_find_minimum_in_rotated_sorted_array.js
@@ -0,0 +1,15 @@
+// Binary Search Solution
+var findMin = function(nums) {
+  var left = 0,
+      right = nums.length - 1
+  
+  while (left < right){
+      var mid = Math.floor((left + right)/2)
+      if (nums[mid] > nums[right]) left = mid + 1
+      else right = mid
+  }
+  return nums[left]
+};
+
+// Time complexity: O(log N)
+// Space Complexity : O(1)
