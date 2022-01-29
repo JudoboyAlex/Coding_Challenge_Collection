663ee1d3ef37b631e7be64de92044c4b935ede9a
Alexander G. Yang <spark8402@rogers.com>
2022-01-28 23:22:20 -0500

    front end interview challenge added

--- Blind_75/tree/104_maximum_depth_of_binary_tree.js
+++ Blind_75/tree/104_maximum_depth_of_binary_tree.js
@@ -1,3 +1,4 @@
+// DFS
 var maxDepth = function(root) {
     if(!root){
         return 0;
@@ -11,3 +12,20 @@ var maxDepth = function(root) {
 };
 
 // Time complexity: O(n)
+
+// BFS 
+var maxDepth = function (root) {
+  if (!root) return 0;
+  const queue = [root];
+  let depth = 0;
+  while (queue.length !== 0) {
+    depth++;
+    const len = queue.length;
+    for (let i = 0; i < len; i++) {
+      if (queue[i].left) queue.push(queue[i].left);
+      if (queue[i].right) queue.push(queue[i].right);
+    }
+    queue.splice(0, len);
+  }
+  return depth;
+};
