449c6cc29e8a5e5d55333b03fc45b7969c535b60
Alexander G. Yang <spark8402@rogers.com>
2022-02-09 18:19:26 -0500

    added another solution for blind 75

--- Blind_75/tree/98_validate_binary_search_tree.js
+++ Blind_75/tree/98_validate_binary_search_tree.js
@@ -6,3 +6,22 @@ var isValidBST = function(root, min=null, max=null) {
 };
 
 // Time: O(N), Space: O(N)
+
+// easy to understand solution using inorder traversal
+var isValidBST = function (root) {
+  let arr = [];
+  var traverse = function (root) {
+    if (!root) return;
+    traverse(root.left);
+    arr.push(root.val);
+    traverse(root.right);
+  };
+
+  traverse(root);
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] >= arr[i + 1]) return false;
+  }
+
+  return true;
+};
