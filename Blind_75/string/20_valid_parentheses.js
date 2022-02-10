8aa3332e3c0b037b80ee7c162243ddd4809d28ba
Alexander G. Yang <spark8402@rogers.com>
2022-02-10 11:35:08 -0500

    added easier to understand solution

--- Blind_75/string/20_valid_parentheses.js
+++ Blind_75/string/20_valid_parentheses.js
@@ -16,3 +16,25 @@ var isValid = function(s) {
 console.log(isValid("([)]"))
 console.log(isValid("()[]{}"))
 console.log(isValid("{[]}"))
+
+//  Time: O(n), Memory: O(n)
+
+var isValid = function (s) {
+  const stack = [];
+  const map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
+  // ([])
+  for (let i = 0; i < s.length; i++) {
+    let c = s[i];
+    if (map[c]) {
+      stack.push(map[c]);
+    } else if (c !== stack.pop()) {
+      return false;
+    }
+  }
+
+  return stack.length === 0;
+};
