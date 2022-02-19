400dfe8d96719b821f148703a71a4d81c9d230ea
Alexander G. Yang <spark8402@rogers.com>
2022-02-18 21:11:15 -0500

    added easier to understand solution

--- Blind_75/array/5_longest_palindromic_substring.js
+++ Blind_75/array/5_longest_palindromic_substring.js
@@ -18,3 +18,50 @@ var longestPalindrome = function(s) {
 
 console.log(longestPalindrome("babad")) // bab
 console.log(longestPalindrome("cbbd")) // bb
+/*
+output - longest palindrome substring
+input - string
+constraints - ?? this will probably be n^2 runtime, 
+- one runthrough to find a palindrome and another to find it at each index
+edge cases -
+- palindrome can have 1 middle character or 2 middle characters
+- we'll have to calculate both at each index
+
+we'll find palindromes starting from the "middle" index,
+using the findLongestFromIndex function
+
+we'll have one iteration to get the longest palindromes at each index,
+the other iteration to actually find the palindrome
+
+after calculating the two palindromes, we'll take the longer
+and compare it to the current longest
+
+at the end, return the longest palindrome
+*/
+var longestPalindrome = function(s) {
+    let longest = '';
+    
+    // this function finds the longest palindrome using the index as the middle
+    const findLongestFromIndex = (str, i, j) => {
+        let palindrome = '';
+        while (i >= 0 && j < str.length && str[i] === str[j]) {
+            palindrome = str.slice(i, j + 1);
+            i--;
+            j++;
+        }
+        return palindrome;
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+		// palindromes using one and two characters as the middle
+        const palindrome1 = findLongestFromIndex(s, i, i);
+        const palindrome2 = findLongestFromIndex(s, i, i + 1);
+        const longerPalindrome = palindrome1.length > palindrome2.length ? palindrome1 : palindrome2;
+        
+        if (longerPalindrome.length > longest.length) {
+            longest = longerPalindrome;
+        }
+    }
+    
+    return longest;
+};
