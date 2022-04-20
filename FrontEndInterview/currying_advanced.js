83ae85d43f14530f9319fad42549868366c880b9
Alexander Yang <spark8402@rogers.com>
2022-04-19 22:07:39 -0400

    updated currying

--- FrontEndInterview/currying_advanced.js
+++ FrontEndInterview/currying_advanced.js
@@ -18,3 +18,13 @@ function add(num1, num2) {
 
 console.log(add(3)()()()(4)); // -> 7
 console.log(add()()()()(10)(12)); // -> 22
+
+//Roadside Coder Solution on Infinite Currying
+function add(a){
+    return function(b){
+        if(b) return add(a + b)
+        return a;
+    };
+}
+
+console.log(add(5)(2)(4)(8)());
