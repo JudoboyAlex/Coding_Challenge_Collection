d08f7a3b4a347cf64c3a02f225208918fd5b85ec
Alexander Yang <spark8402@rogers.com>
2022-04-22 17:52:58 -0400

    updated pipe solution

--- BigFrontEnd/11_pipe.js
+++ BigFrontEnd/11_pipe.js
@@ -4,27 +4,38 @@
  */
 
 // Solution #1
- function pipe(funcs) {
-  return function(arg) {
-     let result = arg
-     for (let func of funcs) {
-       result = func.call(this, result)
-     }
-     return result
-  }
-}
+//  function pipe(funcs) {
+//   return function(arg) {
+//      let result = arg
+//      for (let func of funcs) {
+//        result = func.call(this, result)
+//      }
+//      return result
+//   }
+// }
 
 // Solution #2
 // function pipe(funcs) {
 //   return function(arg) {
 //      return funcs.reduce((result, func) => {
+//        console.log(result);
 //        return func.call(this, result)
 //      }, arg)
 //   }
 // }
-const times = (y) =>  (x) => x * y
-const plus = (y) => (x) => x + y
-const subtract = (y) =>  (x) => x - y
-const divide = (y) => (x) => x / y
+// const times = (y) =>  (x) => x * y
+// const plus = (y) => (x) => x + y
+// const subtract = (y) =>  (x) => x - y
+// const divide = (y) => (x) => x / y
 
-console.log(pipe([times(2),plus(3),times(4)]))
+// console.log(pipe([times(2),plus(3),times(4)]))
+
+// Solution #3
+const times = (y) => (x) => x * y;
+const plus = (y) => (x) => x + y;
+const subtract = (y) => (x) => x - y;
+const divide = (y) => (x) => x / y;
+
+//const pipe = (fns) => (x) => fns.reduce((y, fn) => fn(y), x);
+
+console.log(pipe([times(2), plus(3), times(4)])(2));
