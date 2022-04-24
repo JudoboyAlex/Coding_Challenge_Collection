306baaeaa2c240519c0aef61e846692580f680bc
Alexander Yang <spark8402@rogers.com>
2022-04-24 16:49:50 -0400

    updated debounce with trailing

--- BigFrontEnd/07_implement_debounce_with_leading_and_trailing_option.js
+++ BigFrontEnd/07_implement_debounce_with_leading_and_trailing_option.js
@@ -18,3 +18,37 @@ function debounce(func, wait, option = { leading: false, trailing: true }) {
     }, wait);
   };
 }
+
+// Solution #2
+function debounce(func, wait, option = { leading: false, trailing: true }) {
+  // in basic debounce, we kept only timerId
+  // here, we will keep lastArgs too as we trailing function call with last arguments
+  let timerId = null;
+  let lastArgs = null;
+
+  // if both leading and trailing are false then do nothing.
+  if (!option.leading && !option.trailing) return () => null;
+
+  return function debounced(...args) {
+    // if timer is over and leading is true
+    // then immediately call supplied function
+    // else capture arguments in lastArgs
+    if (!timerId && option.leading) {
+      func.apply(this, args);
+    } else {
+      lastArgs = args;
+    }
+
+    // clear timer so that next call is exactly after `wait` time
+    clearTimeout(timerId);
+
+    timerId = setTimeout(() => {
+      // invoke only if lastArgs is present and trailing is true
+      if (option.trailing && lastArgs) func.apply(this, lastArgs);
+
+      // reset variables as they need to restart new life after calling this function
+      lastArgs = null;
+      timerId = null;
+    }, wait);
+  };
+}
