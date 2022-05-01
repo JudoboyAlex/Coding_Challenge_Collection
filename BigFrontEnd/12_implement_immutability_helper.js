731aa0da811c924775167d4a07e518dcc038f875
Alexander Yang <spark8402@rogers.com>
2022-04-30 22:49:45 -0400

    another bfe completed

--- BigFrontEnd/12_implement_immutability_helper.js
+++ BigFrontEnd/12_implement_immutability_helper.js
@@ -80,18 +80,18 @@ const newState = update(
 */
 
 //3. {$merge: object} merge object to the location
-const state = {
-  a: {
-    b: {
-      c: 1
-    }
-  },
-  d: 2
-}
-const newState = update(
-  state, 
-  {a: {b: { $merge: {e: 5}}}}
-)
+// const state = {
+//   a: {
+//     b: {
+//       c: 1
+//     }
+//   },
+//   d: 2
+// }
+// const newState = update(
+//   state, 
+//   {a: {b: { $merge: {e: 5}}}}
+// )
 /*
 {
   a: {
@@ -109,3 +109,33 @@ const arr = [1, 2, 3, 4]
 const newArr = update(arr, {0: {$apply: (item) => item * 2}})
 // [2, 2, 3, 4]
 console.log(newArr);
+
+// Solution #2- easy to understand solution
+function update(data, command) {
+  for (const [key, value] of Object.entries(command)) {
+    switch (key) {
+      case "$push":
+        return [...data, ...value];
+      case "$set":
+        return value;
+      case "$merge":
+        if (!(data instanceof Object)) {
+          throw Error("bad merge");
+        }
+        return { ...data, ...value };
+      case "$apply":
+        return value(data);
+      default:
+        if (data instanceof Array) {
+          const res = [...data];
+          res[key] = update(data[key], value);
+          return res;
+        } else {
+          return {
+            ...data,
+            [key]: update(data[key], value),
+          };
+        }
+    }
+  }
+}
