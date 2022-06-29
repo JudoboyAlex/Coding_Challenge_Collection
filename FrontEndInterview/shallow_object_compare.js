bf456c3009ab5988dac73d91f716ab21fd7abe04
Alexander Yang <spark8402@rogers.com>
2022-06-28 23:29:50 -0400

    added shallow object compare

---
+++ FrontEndInterview/shallow_object_compare.js
@@ -0,0 +1,28 @@
+// https://dmitripavlutin.com/how-to-compare-objects-in-javascript/
+function shallowEqual(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+  for (let key of keys1) {
+    if (object1[key] !== object2[key]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const hero1 = {
+  name: 'Batman',
+  realName: 'Bruce Wayne'
+};
+const hero2 = {
+  name: 'Batman',
+  realName: 'Bruce Wayne'
+};
+const hero3 = {
+  name: 'Joker'
+};
+shallowEqual(hero1, hero2); // => true
+shallowEqual(hero1, hero3); // => false
