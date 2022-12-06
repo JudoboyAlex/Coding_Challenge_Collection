b7922bac568b49c053e913732a04ff5b747f51d9
Alexander Yang <spark8402@rogers.com>
2022-12-05 22:15:13 -0500

    grinding

--- Blind_75/tree/102_binary_tree_level_order_traversal.js
+++ Blind_75/tree/102_binary_tree_level_order_traversal.js
@@ -23,3 +23,32 @@ var levelOrder = function(root) {
   return result;
 };
 
+// BFS Solution
+var levelOrder = function (root) {
+  if (!root) return [];
+  let queue = [root];
+  let result = [];
+
+  while (queue.length !== 0) {
+    let arr = [];
+    const queueLength = queue.length;
+
+    for (let i = 0; i < queueLength; i++) {
+      let current = queue.shift();
+      if (current.left) queue.push(current.left);
+      if (current.right) queue.push(current.right);
+      arr.push(current.val);
+    }
+
+    result.push(arr);
+  }
+  return result;
+};
+
+/*
+Complexity Analysis
+
+Time complexity : O(N) since each node is processed exactly once.
+
+Space complexity : O(N) to keep the output structure which contains N node values.
+*/
