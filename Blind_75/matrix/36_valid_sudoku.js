13911b23903573b62d025c302052de50cee26549
Alexander Yang <spark8402@rogers.com>
2022-12-18 16:19:28 -0500

    grinding after world cup final

---
+++ Blind_75/matrix/36_valid_sudoku.js
@@ -0,0 +1,34 @@
+var isValidSudoku = function (board) {
+  const set = new Set();
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      const cell = board[i][j];
+      //   console.log(cell);
+      if (cell === ".") continue;
+      const boxNum = 3 * Math.floor(i / 3) + Math.floor(j / 3);
+      console.log(boxNum);
+      const row = `row: ${i}, value ${cell}`;
+      const col = `col: ${j}, value ${cell}`;
+      const box = `box: ${boxNum}, value ${cell}`;
+      if (set.has(row) || set.has(col) || set.has(box)) return false;
+      set.add(row);
+      set.add(col);
+      set.add(box);
+    }
+  }
+  return true;
+};
+
+/*
+Complexity Analysis
+
+Let N be the board length, which is 9 in this question. Note that since the value of N is fixed, the time and space complexity of this algorithm can be interpreted as O(1). However, to better compare each of the presented approaches, we will treat NNN as an arbitrary value in the complexity analysis below.
+
+Time complexity: O(N^2)
+ because we need to traverse every position in the board, and each of the four check steps is an   
+ O(1) operation.
+
+Space complexity: O(N^2) 
+ because in the worst-case scenario, if the board is full, we need a hash set each with size N to store all seen numbers for each of the N rows, N columns, and N boxes, respectively.
+*/
