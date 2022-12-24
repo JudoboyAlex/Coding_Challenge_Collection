378bb97720aa7c412cbd2ac4822e7699f9381d8e
Alexander Yang <spark8402@rogers.com>
2022-12-24 11:31:31 -0500

    grind christmas

--- Leetcode/289_game_of_life.js
+++ Leetcode/289_game_of_life.js
@@ -54,3 +54,42 @@ Time Complexity: O(M×N), where M is the number of rows and N is the number of c
 
 Space Complexity: O(1)
 */
+
+// Cleaner Solution
+var gameOfLife2 = function (board) {
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      let cell = board[i][j];
+      let neighbors = getNeighbors(i, j, board);
+      // cell went from 0 to 1
+      if (cell == 0 && neighbors == 3) {
+        board[i][j] = 2;
+      }
+      // cell went from 1 to 0
+      if (cell == 1 && (neighbors < 2 || neighbors > 3)) {
+        board[i][j] = -1;
+      }
+    }
+  }
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+      if (board[i][j] == -1) board[i][j] = 0;
+      if (board[i][j] == 2) board[i][j] = 1;
+    }
+  }
+};
+
+var getNeighbors = function (r, c, board) {
+  let radius = [-1, 0, +1],
+    count = 0;
+  for (let i = 0; i < radius.length; i++) {
+    for (let j = 0; j < radius.length; j++) {
+      console.log(r + radius[i]);
+      if (!(radius[i] == 0 && radius[j] == 0) && board[r + radius[i]] != null) {
+        let neighbor = board[r + radius[i]][c + radius[j]];
+        if (Math.abs(neighbor) == 1) count += 1;
+      }
+    }
+  }
+  return count;
+};
