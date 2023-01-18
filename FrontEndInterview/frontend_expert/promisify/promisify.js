44c9c587e674f61c766a382c20df35e6b6f2908d
Alexander Yang <spark8402@rogers.com>
2023-01-17 21:28:06 -0500

    gridning

---
+++ FrontEndInterview/frontend_expert/promisify/promisify.js
@@ -0,0 +1,36 @@
+function promisify(callback) {
+  return function (...args) {
+    return new Promise((resolve, reject) => {
+      function handleErrorAndValue(error, value) {
+        if (error == null) {
+          resolve(value);
+        } else {
+          reject(error);
+        }
+      }
+      callback.call(this, ...args, handleErrorAndValue);
+    });
+  };
+}
+
+function adder(x, y, handleErrorAndValue) {
+  const value = x + y;
+  if (typeof value !== "number") {
+    const error = new Error("Not a number");
+    handleErrorAndValue(error, null);
+  } else {
+    handleErrorAndValue(null, value);
+  }
+}
+
+const promisifiedAdder = promisify(adder);
+
+promisifiedAdder(1, 2)
+  .then(console.log) // This would log 3.
+  .catch(console.error);
+
+promisifiedAdder(1, "foobar").then(console.log).catch(console.error); // An error would be caught and logged.
+
+const object = {};
+object.promisifiedAdder = promisify(adder);
+object.promisifiedAdder();
