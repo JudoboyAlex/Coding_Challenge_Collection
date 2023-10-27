0b82fff78ea9ed0be9456e6b1aa61f86d0526cfe
Alexander Yang <spark8402@rogers.com>
2023-10-26 21:36:26 -0400

    grinding

--- FrontEndInterview/leetcode_javascript/2622_cache_with_time_limit.js
+++ FrontEndInterview/leetcode_javascript/2622_cache_with_time_limit.js
@@ -68,3 +68,26 @@ TimeLimitedCache.prototype.count = function() {
  * timeLimitedCache.get(1) // 42
  * timeLimitedCache.count() // 1
  */
+
+// Book Solution
+class TimeLimitedCache {
+  cache = new Map();
+
+  set(key, value, duration) {
+    const valueInCache = this.cache.get(key);
+    if (valueInCache) {
+      clearTimeout(valueInCache.timeout);
+    }
+    const timeout = setTimeout(() => this.cache.delete(key), duration);
+    this.cache.set(key, { value, timeout });
+    return Boolean(valueInCache);
+  }
+
+  get(key) {
+    return this.cache.has(key) ? this.cache.get(key).value : -1;
+  }
+
+  count() {
+    return this.cache.size;
+  }
+};
