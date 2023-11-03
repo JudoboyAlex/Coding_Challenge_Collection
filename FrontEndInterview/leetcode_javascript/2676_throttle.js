0dc9b47b1afe0e420ed261bd4c2aec0778788324
Alexander Yang <spark8402@rogers.com>
2023-11-02 21:51:20 -0400

    grinding

--- FrontEndInterview/leetcode_javascript/2676_throttle.js
+++ FrontEndInterview/leetcode_javascript/2676_throttle.js
@@ -24,3 +24,17 @@ var throttle = function (fn, t) {
  * throttled("log"); // logged immediately.
  * throttled("log"); // logged at t=100ms.
  */
+
+// Date.now solution
+var throttle = function (fn, t) {
+  let timeout = null;
+  let nextTimeToCallFn = 0;
+  return function (...args) {
+    const delay = Math.max(0, nextTimeToCallFn - Date.now());
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      fn(...args);
+      nextTimeToCallFn = Date.now() + t;
+    }, delay);
+  };
+};
