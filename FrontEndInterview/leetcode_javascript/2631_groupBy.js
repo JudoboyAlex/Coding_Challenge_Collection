bd1cf9d5ba13a90cf74a4c707ba6a77f701103cc
Alexander Yang <spark8402@rogers.com>
2023-11-04 09:21:37 -0400

    grinding

---
+++ FrontEndInterview/leetcode_javascript/2631_groupBy.js
@@ -0,0 +1,20 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function (fn) {
+  const returnObject = {};
+  for (const item of this) {
+    const key = fn(item);
+    if (key in returnObject) {
+      returnObject[key].push(item);
+    } else {
+      returnObject[key] = [item];
+    }
+  }
+  return returnObject;
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
